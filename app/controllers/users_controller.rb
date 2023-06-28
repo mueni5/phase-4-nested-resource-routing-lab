b14f054ab3c4ec42e7bdb7750cf5b637fbe9851b
+ <+>
2023-06-28 14:40:14 +0300

    Done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,20 @@
 class UsersController < ApplicationController
 
+  def index
+    users = User.all
+    render json: users, include: :items
+  end
+
   def show
     user = User.find_by(id: params[:id])
     render json: user, include: :items
   end
 
+  private
+
+  def render_not_found_response
+    render json: { error: "User not found" }, status: :not_found
+  end
 end
+
+
